f9b4b8d5e2d96263bce6b78eb9ceb66246942fe5
Yogesh Chhabra <37286430+yogeshchhabra99@users.noreply.github.com>
2018-05-25 16:13:11 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,61 @@
+import io
+import os
+import sys
+
+# Imports the Google Cloud client library
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+
+
+def writesubs( str_out_file):
+	# Instantiates a client
+	client = speech.SpeechClient()
+
+	# The name of the audio file to transcribe
+	file_name = os.path.join(
+	    os.path.dirname(__file__),
+	    'resources',
+	    'outaudio.wav')
+
+	# Loads the audio into memory
+	with io.open(file_name, 'rb') as audio_file:
+	    content = audio_file.read()
+	    audio = types.RecognitionAudio(content=content)
+
+	config = types.RecognitionConfig(
+	    encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+	    sample_rate_hertz=40000,		#take care of the sampling rate
+	    language_code='en-US',
+	    enable_word_time_offsets=True)
+
+	# Detects speech in the audio file
+	operation = client.long_running_recognize(config, audio)
+
+	print('Waiting for operation to complete...')
+	result = operation.result(timeout=90)
+
+	file=open("{}".format(str_out_file),"w")
+
+	for result in result.results:
+	       alternative = result.alternatives[0]
+	  #      print(u'Transcript: {}'.format(alternative.transcript))
+	   #     print('Confidence: {}'.format(alternative.confidence))
+	i=1
+	for word_info in alternative.words:
+	            word = word_info.word
+	            start_time = word_info.start_time
+	            end_time = word_info.end_time
+	            #print('Word: {}, start_time: {}, end_time: {}'.format(
+	             #   word,
+	              #  start_time.seconds + start_time.nanos * 1e-9,
+	               # end_time.seconds + end_time.nanos * 1e-9))
+	            file.write("{}\n".format(i))
+	            file.write("{} --> {}\n".format(start_time.seconds + start_time.nanos * 1e-9,
+	                end_time.seconds + end_time.nanos * 1e-9))
+	            file.write("{}\n\n".format(word))
+	            i=i+1
+
+writesubs(sys.argv[1])
+
+
---
+++ write_subtitles.h
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<stdlib.h>
+#include<string>
+#include<string.h>
+
+using namespace std;
+
+int writesubs(const char* video_file_name,const char* out_file_name)
+{
+	string str="ffmpeg -i ";
+	str=str + video_file_name +" -ar 40000 -ac 1 resources/outaudio.wav";  //output audio with 40000 sampling rate and single channel
+	int i=system(str.c_str());			//calling the ffmpeg command
+	string str2="python script.py " ;
+	str2=str2 + out_file_name;				
+	system(str2.c_str());						//calling the python script using system command
+	string str3= "rm -r -f resources/outaudio.wav";
+	system(str3.c_str());
+	return 0;
+	
+} 
+	//if command runs system() returns 0 else something else
