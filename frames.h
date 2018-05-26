881bf258ebb79468c47c1f02a7da508cb9b7dffb
Yogesh Chhabra <chhabra99yogesh@gmail.com>
2018-05-26 14:55:15 +0530

    now i downt need to send directory

--- frames.h
+++ frames.h
@@ -5,12 +5,12 @@
 
 using namespace std;
 
-int saveframes(string video_file_path,string no_of_frames,string time,string word,string directory)
+int saveframes(string video_file_path,string no_of_frames,string time,string word)//,string directory)
 {
 	string str="ffmpeg -i ";
 	str=str + video_file_path + " -ss ";
 	str=str + "00:00:" + time + " -vframes ";
-	str=str+ no_of_frames + " -vf fps=10 "+directory+"frames/outimg_:" + word + "_" + time + "seconds_%d.jpg";
+	str=str+ no_of_frames + " -vf fps=10 "/*+directory*/+"frames/outimg_:" + word + "_" + time + "seconds_%d.jpg";
 	//cout << str;
 	system(str.c_str());
 	return 0;
