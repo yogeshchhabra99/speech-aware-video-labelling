339450a9edb2050b4d932ff8ea0be20b99958e5e
Yogesh Chhabra <chhabra99yogesh@gmail.com>
2018-05-26 11:08:56 +0530

    save frames now needs a parameter output directory

--- frames.h
+++ frames.h
@@ -5,13 +5,15 @@
 
 using namespace std;
 
-int saveframes(string video_file_path,string no_of_frames,string time,string word)
+int saveframes(string video_file_path,string no_of_frames,string time,string word,string directory)
 {
 	string str="ffmpeg -i ";
 	str=str + video_file_path + " -ss ";
 	str=str + "00:00:" + time + " -vframes ";
-	str=str+ no_of_frames + " -vf fps=10 outimg_:" + word + "_" + time + "seconds_%d.jpg";
+	str=str+ no_of_frames + " -vf fps=10 "+directory+"frames/outimg_:" + word + "_" + time + "seconds_%d.jpg";
 	//cout << str;
 	system(str.c_str());
 	return 0;
 }
+
+// I am assuming that the directory string has a / in the end if not then insert.
